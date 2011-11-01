34b2c882abba433591ad32d3f855814a0dbe6914
Keith Salisbury <keithsalisbury@gmail.com>
2011-11-01 21:40:38 +0000

    only print out properties, not associations when calling to_xml on ConstantContact::List class

--- lib/constant_contact/list.rb
+++ lib/constant_contact/list.rb
@@ -6,7 +6,9 @@ module ConstantContact
     def to_xml
       xml = Builder::XmlMarkup.new
       xml.tag!("ContactList", :xmlns => "http://ws.constantcontact.com/ns/1.0/") do
-        self.attributes.each{|k, v| xml.tag!( k.to_s.camelize, v )}
+        self.attributes.each do |k, v| 
+          xml.tag!( k.to_s.camelize, v ) unless v.respond_to?(:to_xml)
+        end
       end
     end
 
